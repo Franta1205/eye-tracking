d2dbea98e628d24818ad7ee943fa7732e115bb2d
Franta1205 <hoza.fr@gmail.com>
2025-08-13 18:42:34 +0200

    building profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
 
   private
 
   def layout_by_resource
     devise_controller? ? 'devise' : 'application'
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :profile
+  
+  attr_accessor :first_name, :last_name
+  
+  after_create :create_profile
+  
+  private
+  
+  def create_profile
+    self.build_profile(first_name: first_name, last_name: last_name).save
+  end
 end
