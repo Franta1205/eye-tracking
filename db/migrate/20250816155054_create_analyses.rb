b54fe09427be2b0e13b01f71516d6018ffd0e26e
Franta1205 <hoza.fr@gmail.com>
2025-08-16 19:09:53 +0200

    fixing migration

--- db/migrate/20250816155054_create_analyses.rb
+++ db/migrate/20250816155054_create_analyses.rb
@@ -5,8 +5,10 @@ class CreateAnalyses < ActiveRecord::Migration[7.1]
       t.string :name, null: false
       t.integer :participants, null: false
       t.integer :analysis_type, null: false
-      t.string :image
+      t.string :original_image
       t.text :goals
+      t.string :heat_map
+      t.text :ai_feedback
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_16_155054) do
     t.string "name", null: false
     t.integer "participants", null: false
     t.integer "analysis_type", null: false
-    t.string "image"
+    t.string "original_image"
     t.text "goals"
+    t.string "heat_map"
+    t.text "ai_feedback"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_analyses_on_user_id"
